1372261d3133dba253b6b48468d219308932420b
Björn Perrech <b.perrech@macworx.de>
2018-06-25 13:47:02 +0200

    demote script und namensänderung

--- demote_user.sh
+++ demote_user.sh
@@ -9,23 +9,23 @@ fi
 
 # if argument username not empty
 if [[ ! "$USERNAME" ]]; then
-  echo "no username given!"
+  echo "No username given!"
   exit 1
 else
 #  if username is not member of group admin
-  echo "first we check if a user '${USERNAME}' exists on the system"
+  echo "First we check if a user '${USERNAME}' exists on the system"
   USER_EXISTS=$(dscl . list /users | grep -F ${USERNAME})
   if [[ ! "$USER_EXISTS" ]]; then
     echo "user '${USERNAME}' does not exist"
     exit 1
   fi
-  echo "checking for admin group membership"
+  echo "Checking for admin group membership"
   IS_MEMBER=$(dscl . -read /groups/admin GroupMembership | grep -F ${USERNAME})
-  if [[ ! "$IS_MEMBER" ]]; then
-    echo "${USERNAME} is not member of the admin group. appending group membership"
-    dscl . -append /groups/admin GroupMembership ${USERNAME}
+  if [[ "$IS_MEMBER" ]]; then
+    echo "${USERNAME} is a member of the admin group. Deleting group membership."
+    dscl . -delete /groups/admin GroupMembership ${USERNAME}
   else
-    echo "${USERNAME} is already a member of the admin group. skipping."
+    echo "${USERNAME} is not a member of the admin group. Skipping."
   fi
 fi
 exit 0
